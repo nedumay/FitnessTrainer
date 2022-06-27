a383304e6d9b727249bcc85ec52a460fb7e48c44
Samim <Samim058@gamil.com>
2022-06-27 22:25:26 +0300

    New activity

--- app/src/main/java/com/example/testproject/ui/registration/RegistrationActivity.kt
+++ app/src/main/java/com/example/testproject/ui/registration/RegistrationActivity.kt
@@ -3,6 +3,7 @@ package com.example.testproject.ui.registration
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import com.example.testproject.R
 import com.example.testproject.ui.login.LoginActivity
@@ -14,10 +15,16 @@ class RegistrationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_registration)
 
         val backView: ImageView = findViewById(R.id.arrow_back)
+        val nextButton: Button = findViewById(R.id.next_btn)
 
         backView.setOnClickListener {
             val intentLogin = Intent(this@RegistrationActivity, LoginActivity::class.java)
             startActivity(intentLogin)
+        }
+
+        nextButton.setOnClickListener {
+            val intentRegTwo = Intent(this@RegistrationActivity, RegistrationTwo::class.java)
+            startActivity(intentRegTwo)
         }
     }
 }
