ec034fab9c452ac984e9061f92e89484b3982ebb
Samim <Samim058@gmail.com>
2024-07-25 16:37:07 +0300

    Refactored API

--- app/src/main/java/com/example/testproject/app/data/network/ApiService.kt
+++ app/src/main/java/com/example/testproject/app/data/network/ApiService.kt
@@ -6,14 +6,14 @@ import retrofit2.http.GET
 interface ApiService {
 
     // Beginner exercise API
-    @GET("6cdda04a-2ace-4338-a4be-a93a09d0552f")
+    @GET("4e4399ba-7eef-4fdc-b877-f457037e0fb9")
     suspend fun getBeginnerInfo(): ListLvlDto
 
     // Continuing exercise API
-    @GET("7f19581d-524a-4585-84be-2a24f0ea4b33")
+    @GET("f5732aed-48f0-450c-a9f9-24bdb06c5b7a")
     suspend fun getContinuingInfo(): ListLvlDto
 
     //Advanced exercise API
-    @GET("7dff7fce-8272-492b-b837-431fd2f2220a")
+    @GET("fbf0dbb0-57d1-4d43-bd56-c4e1f7fd4e29")
     suspend fun getAdvancedInfo(): ListLvlDto
 }
