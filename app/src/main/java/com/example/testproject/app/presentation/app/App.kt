5ecc21cde57f612f72ab05efbf9c7f2e4bba24c9
Samim <Samim058@gmail.com>
2024-08-26 14:47:12 +0300

    version code 5

--- app/src/main/java/com/example/testproject/app/presentation/app/App.kt
+++ app/src/main/java/com/example/testproject/app/presentation/app/App.kt
@@ -1,18 +1,9 @@
 package com.example.testproject.app.presentation.app
 
 import android.app.Application
-import android.app.NotificationManager
-import android.content.BroadcastReceiver
-import android.content.IntentFilter
-import android.util.Log
-import com.example.testproject.R
 import com.example.testproject.app.di.DaggerApplicationComponent
-import com.example.testproject.app.utils.MyNotification
-import com.example.testproject.app.utils.NotificationReceiver
-import com.google.firebase.FirebaseApp
-import javax.inject.Inject
 
-class App : Application(){
+class App : Application() {
 
     val component by lazy {
         DaggerApplicationComponent.factory().create(this)
