877f1f5a649ced808c61810c9a4e38306bc72ad8
Samim <samim058@gmail.com>
2026-02-25 23:01:59 +0300

    Пока что убрал тесты

--- app/src/test/java/com/example/testproject/TestApiService.kt
+++ app/src/test/java/com/example/testproject/TestApiService.kt
@@ -21,97 +21,97 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class TestApiService {
 
-    private lateinit var retrofit: Retrofit
-    private lateinit var apiService: ApiService
-
-    @Before
-    fun setup() {
-        retrofit = Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl("https://api.myjson.online/v1/records/")
-            .build()
-
-        apiService = mock(ApiService::class.java)
-
-    }
-
-    @Test
-    fun testGetBeginnerInfo() = runBlocking {
-        val exercise = Exercise(
-            id = 1,
-            name = "name",
-            subtitle = "subtitle",
-            video = "video",
-            desc = "desc",
-            area = "area",
-            areaImg = "areaImg"
-        )
-        val workout = Workout(
-            id = 1,
-            title = "title",
-            picture = "picture",
-            exercise = listOf(exercise)
-        )
-        val listLvlDto = ListLvlDto(listLvl = listOf(workout))
-        val response = ApiResponse(listLvlDto, "id", "displayName", 1)
-        Mockito.`when`(apiService.getBeginnerInfo()).thenReturn(response)
-
-        val result = apiService.getBeginnerInfo()
-
-        assertEquals(response, result)
-    }
-
-    @Test
-    fun testGetContinuingInfo() = runBlocking {
-        val exercise = Exercise(
-            id = 1,
-            name = "name",
-            subtitle = "subtitle",
-            video = "video",
-            desc = "desc",
-            area = "area",
-            areaImg = "areaImg"
-        )
-        val workout = Workout(
-            id = 1,
-            title = "title",
-            picture = "picture",
-            exercise = listOf(exercise)
-        )
-        val listLvlDto = ListLvlDto(listLvl = listOf(workout))
-        val response = ApiResponse(listLvlDto, "id", "displayName", 1)
-        Mockito.`when`(apiService.getContinuingInfo()).thenReturn(response)
-
-        // Вызов метода
-        val result = apiService.getContinuingInfo()
-
-        // Проверка результата
-        assertEquals(response, result)
-    }
-
-    @Test
-    fun testGetAdvancedInfo() = runBlocking {
-        val exercise = Exercise(
-            id = 1,
-            name = "name",
-            subtitle = "subtitle",
-            video = "video",
-            desc = "desc",
-            area = "area",
-            areaImg = "areaImg"
-        )
-        val workout = Workout(
-            id = 1,
-            title = "title",
-            picture = "picture",
-            exercise = listOf(exercise)
-        )
-        val listLvlDto = ListLvlDto(listLvl = listOf(workout))
-        val response = ApiResponse(listLvlDto, "id", "displayName", 1)
-        Mockito.`when`(apiService.getAdvancedInfo()).thenReturn(response)
-
-        val result = apiService.getAdvancedInfo()
-
-        assertEquals(response, result)
-    }
+//    private lateinit var retrofit: Retrofit
+//    private lateinit var apiService: ApiService
+//
+//    @Before
+//    fun setup() {
+//        retrofit = Retrofit.Builder()
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .baseUrl("https://api.myjson.online/v1/records/")
+//            .build()
+//
+//        apiService = mock(ApiService::class.java)
+//
+//    }
+//
+//    @Test
+//    fun testGetBeginnerInfo() = runBlocking {
+//        val exercise = Exercise(
+//            id = 1,
+//            name = "name",
+//            subtitle = "subtitle",
+//            video = "video",
+//            desc = "desc",
+//            area = "area",
+//            areaImg = "areaImg"
+//        )
+//        val workout = Workout(
+//            id = 1,
+//            title = "title",
+//            picture = "picture",
+//            exercise = listOf(exercise)
+//        )
+//        val listLvlDto = ListLvlDto(listLvl = listOf(workout))
+//        val response = ApiResponse(listLvlDto, "id", "displayName", 1)
+//        Mockito.`when`(apiService.getBeginnerInfo()).thenReturn(response)
+//
+//        val result = apiService.getBeginnerInfo()
+//
+//        assertEquals(response, result)
+//    }
+//
+//    @Test
+//    fun testGetContinuingInfo() = runBlocking {
+//        val exercise = Exercise(
+//            id = 1,
+//            name = "name",
+//            subtitle = "subtitle",
+//            video = "video",
+//            desc = "desc",
+//            area = "area",
+//            areaImg = "areaImg"
+//        )
+//        val workout = Workout(
+//            id = 1,
+//            title = "title",
+//            picture = "picture",
+//            exercise = listOf(exercise)
+//        )
+//        val listLvlDto = ListLvlDto(listLvl = listOf(workout))
+//        val response = ApiResponse(listLvlDto, "id", "displayName", 1)
+//        Mockito.`when`(apiService.getContinuingInfo()).thenReturn(response)
+//
+//        // Вызов метода
+//        val result = apiService.getContinuingInfo()
+//
+//        // Проверка результата
+//        assertEquals(response, result)
+//    }
+//
+//    @Test
+//    fun testGetAdvancedInfo() = runBlocking {
+//        val exercise = Exercise(
+//            id = 1,
+//            name = "name",
+//            subtitle = "subtitle",
+//            video = "video",
+//            desc = "desc",
+//            area = "area",
+//            areaImg = "areaImg"
+//        )
+//        val workout = Workout(
+//            id = 1,
+//            title = "title",
+//            picture = "picture",
+//            exercise = listOf(exercise)
+//        )
+//        val listLvlDto = ListLvlDto(listLvl = listOf(workout))
+//        val response = ApiResponse(listLvlDto, "id", "displayName", 1)
+//        Mockito.`when`(apiService.getAdvancedInfo()).thenReturn(response)
+//
+//        val result = apiService.getAdvancedInfo()
+//
+//        assertEquals(response, result)
+//    }
 }
